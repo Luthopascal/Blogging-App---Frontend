c4846a75e03e136b014a717a86a983ee34e5a732
Lutho Dodo <53383214+Luthopascal@users.noreply.github.com>
2025-11-14 00:33:33 +0200

    Rename CSS import to match file case

--- src/pages/editPost.jsx
+++ src/pages/editPost.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAuth } from '../lib/Authcontext';
 import { useParams, useNavigate } from 'react-router-dom';
-import './EditPost.css';
+import './editPost.css';
 
 const EditPost = () => {
   const { id } = useParams(); // Get post ID from URL
